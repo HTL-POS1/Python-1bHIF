25bbd83056eb1529cf29cac2c3e1ca2a5ec5b43c
forumat <forumat.kontakt@gmail.com>
2021-11-29 12:08:16 +0100

    finished digits.py

--- Digits.py
+++ Digits.py
@@ -1,10 +1,11 @@
 # Ausfindung der Ziffern einer Zahl
 # 1BHIF | Marc Edlinger | 25.11.2021
 target = int(input("Gimme eine Zahl: "))  # number, you want to get the digits
-digits = 0     # amount of all digits
+digits = []                               # list with all digits
+backup_target = target                    # unmodified target number
 
-while target != 0:
-    digits += 1
+while target >= 1:
+    digits.append(target % 10)
     target //= 10
 
-print(digits)
+print("Ziffern der Zahl", backup_target, "=", digits)
