18753beb2cf0a777f42ef1ee6997da89c9e97a93
forumat <forumat.kontakt@gmail.com>
2021-12-02 09:40:55 +0100

    started and finished 1x1.py

---
+++ 1x1.py
@@ -0,0 +1,14 @@
+# 1x1 von allen Zahlen von 2 bis zu einer beliebigen
+# 1BHIF | Marc Edlinger | 02.12.2021
+max = int(input("Bis wie weit willst du das 1x1 berechnen? "))
+
+
+def calculate_multiplication_row(current_step: int):
+    display = ""
+    for i in range(1, 11):
+        display = display + str(i) + " * " + str(current_step) + " = " + str(current_step * i) + " | "
+    return display
+
+for i in range(max):
+    print(calculate_multiplication_row(i))
+
