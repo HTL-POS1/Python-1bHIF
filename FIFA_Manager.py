76e378285280b8573bb501eb641a2dfe3ac413a6
Forumat <forumat.kontakt@gmail.com>
2021-12-03 18:09:12 +0100

    finished FIFA_Manager.py and added utility module utils.py

--- FIFA_Manager.py
+++ FIFA_Manager.py
@@ -1,9 +1,12 @@
 # FIFA-Manager, vergleich verschiedener Mannschaften
 # 1BHIF | Marc Edlinger | 02.12.2021
-rounds = 0                        # max rounds
-max_goals = None                  # worst team, the main team played with
-max_goals_opposite = None         # best team, the main team played with
-opposite_team = None              # Current opposite team
+import sys
+import util.utils as u
+
+rounds = 0                         # max rounds
+worst_team = None                  # worst team, the main team played with
+best_team = None                   # best team, the main team played with
+
 
 class Team():       # model for a team, storing their stats
 
@@ -15,29 +18,17 @@ class Team():       # model for a team, storing their stats
         self.looses = 0
         self.draws = 0
 
-    def __init__(self, name, goals):
-        self.name = name
-        self.goals = goals
-        self.goals_taken = 0
-        self.wins = 0
-        self.looses = 0
-        self.draws = 0
 
 default_team = Team(input("Wie heißt dein Team? "))     # main team
-
-while (rounds <= 0):
-    rounds = int(input("Wie viele Runden werden gespielt? "))
-
+rounds = u.ask_for_positive_int("Wie viele Runden werden gespielt? (0 = Ende) ")
+if (rounds == 0):
+    sys.exit(505)
 
 for i in range(rounds):
-    global opposite_team
-    global max_goals
-    global max_goals_opposite
-
     # inputs
-    opposite_team = Team(input("Wie heißt der Gegner für Runde", i, "? "))
-    goals_made = int(input("Wie viele Tore hat das Team", default_team.name, "gemacht? "))
-    goals_taken = int(input("Wie viele Tore hat das Team", opposite_team.name, "gemacht? "))
+    opposite_team = Team(input("Wie heißt der Gegner für Runde " + str(i+1) + "? "))
+    goals_made = u.ask_for_positive_int("Wie viele Tore hat das Team " + default_team.name + " gemacht? ")
+    goals_taken = u.ask_for_positive_int("Wie viele Tore hat das Team " + opposite_team.name + " gemacht? ")
 
     # save goals
     default_team.goals += goals_made
@@ -47,11 +38,11 @@ for i in range(rounds):
     opposite_team.goals_taken = goals_made
 
     # record check
-    if ((max_goals is None) or (goals_made > max_goals.goals)):
-        max_goals = opposite_team
+    if ((worst_team is None) or (goals_made > worst_team.goals_taken)):
+        worst_team = opposite_team
 
-    if ((max_goals_opposite is None) or (goals_taken > max_goals_opposite.goals)):
-        max_goals_opposite = opposite_team
+    if ((best_team is None) or (goals_made > best_team.goals)):
+        best_team = opposite_team
 
     # win check
     if (goals_made > goals_taken):
@@ -67,8 +58,8 @@ for i in range(rounds):
         default_team.draws += 1
         print("Unentschieden :/")
 
-print(rounds, "Spiele,", default_team.wins, "Gewonnen,", default_team.looses, "Verloren,", default_teams.draws, "Unentschieden")
-print("Tordifferenz:", (default_team.goals_made - default_team.goals_taken))
-print("Bester Sieg:", max_goals.name, ", Tore:", max_goals.goals_taken)
-print("Schlechteste Niederlage:", max_goals_opposite.name, ", Tore:", max_goals_opposite.goals_made)
+print(rounds, "Spiele,", default_team.wins, "Gewonnen,", default_team.looses, "Verloren,", default_team.draws, "Unentschieden")
+print("negatives Torverhältnis von:", default_team.goals, "zu", default_team.goals_taken)
+print("Bester Sieg:", worst_team.name, ", Tore:", worst_team.goals_taken)
+print("Schlechteste Niederlage:", best_team.name, ", Tore:", best_team.goals)
 
