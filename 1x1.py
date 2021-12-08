7b955ddc63a3d666171c00ecdba0fcb8ff62c56c
Forumat <82802406+Forumat@users.noreply.github.com>
2021-12-08 11:00:33 +0100

    Update 1x1.py
    made new line on end of a multiplication row

--- 1x1.py
+++ 1x1.py
@@ -6,7 +6,7 @@ max = int(input("Bis wie weit willst du das 1x1 berechnen? "))
 def calculate_multiplication_row(current_step: int):
     display = ""
     for i in range(1, 11):
-        display = display + str(i) + " * " + str(current_step) + " = " + str(current_step * i) + " | "
+        display = display + str(i) + " * " + str(current_step) + " = " + str(current_step * i) + "\n"
     return display
 
 for i in range(1, max + 1):
