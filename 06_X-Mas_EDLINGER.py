67d89694b6d2a3411a8dc349854af4b5253b76f9
Forumat <forumat.kontakt@gmail.com>
2021-12-15 20:47:43 +0100

    worked a little bit on the xmas tree

--- 06_X-Mas_EDLINGER.py
+++ 06_X-Mas_EDLINGER.py
@@ -1,7 +1,7 @@
 # Christbaum Zeichnen
 # 1BHIF | Marc Edlinger | 13.12.2021
 def draw_vertical(tree_height: int):
-    print(tree_height * " " + "I")
+    print(tree_height * " " + "I")                      # the top of the tree
 
     # print the leaves of the tree
     for i in range(1, tree_height + 1):
@@ -14,31 +14,27 @@ def draw_vertical(tree_height: int):
             placeholders = " " + placeholders
         print(spaces + placeholders)
 
-    # build the left part of first line of the log, with spheres
-    log_string: str = " "
-    switch: bool = True
-    for i in range(tree_height - 1):
-        if (switch is True):
-            log_string += "o"
-            switch = False
-            continue
-        log_string += " "
-        switch = True
-    log_string += "H"
-    if (tree_height % 2 != 0):                          # if the height is odd, you have to add a space
+    # print out the log
+    log_string: str = ""
+    for i in range(tree_height * 2):
         log_string += " "
 
-    # build the right part of first line of the log, with spheres
-    switch = True
-    for i in range(tree_height - 1):
-        if (switch is True):
-            log_string += "o"
-            switch = False
-            continue
-        log_string += " "
-        switch = True
+    log_string = interleave(log_string, "o", 2)                                 # insert the o's at every second char
+    log_string = log_string[:tree_height] + "H" + log_string[tree_height + 1:]  # insert the H in the middle
     print(log_string)
-    print(" " * (tree_height) + "H")
+    print(log_string.replace("o", " "))
+
+
+# origin = the original string, you want to interleave
+# symbole = the symbole you want to insert
+# step = the frequence, you want to insert the given smybole in the origin string
+def interleave(origin: str, symbole: str, step: int):
+    for i in range(1, len(origin)+1):
+        # if i is a divider of step. eg. for step = 3, the valid
+        # i values would be 3, 6, 9, 12 ect.
+        if (i % step == 0):
+            origin = origin[:i-1] + symbole + origin[i:]        # insert the symbole
+    return origin
 
 
 def draw_horizontal(tree_height: int):
@@ -72,4 +68,4 @@ def start():                                                # recursive method t
     start()
 
 
-start()
+start()         # first call of recursive method
