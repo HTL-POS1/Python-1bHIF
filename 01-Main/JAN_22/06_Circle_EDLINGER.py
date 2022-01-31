23a323aea6e2242bf20906921f0fd47a2e4f5424
Forumat <forumat.kontakt@gmail.com>
2022-01-31 17:48:37 +0100

    reworked circle file

--- 01-Main/JAN_22/06_Circle_EDLINGER.py
+++ 01-Main/JAN_22/06_Circle_EDLINGER.py
@@ -2,16 +2,12 @@
 # 1BHIF | Marc Edlinger | 31.01.2022
 size: int = 15
 rad: int = 6
-values: list = [[" " for x in range(size)] for y in range(size)]       # fill the array with space strings
+values: list = [[" " for x in range(size)] for y in range(size)]  # fill the array with space strings
 
-def print_circle():
-    for y in range(size):  # put valid y,x values in the map
-        for x in range(size):
-            if ((x - rad) ** 2 + (y - rad) ** 2 <= rad ** 2 - 1):
-                values[y][x] = "*"
+for y in range(size):  # put valid y,x values in the map
+    for x in range(size):
+        if ((x - rad) ** 2 + (y - rad) ** 2 <= rad ** 2 - 1):
+            values[y][x] = "*"
 
-    for line in values:
-        print(' '.join(line))
-
-
-print_circle()
+for line in values:
+    print(' '.join(line))
