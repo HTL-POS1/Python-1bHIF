235e4b5b23228555069f15b40c3ca8a9c99ed00c
Forumat <forumat.kontakt@gmail.com>
2022-02-02 20:41:01 +0100

    tried circle

--- 01-Main/JAN_22/06_Circle_EDLINGER.py
+++ 01-Main/JAN_22/06_Circle_EDLINGER.py
@@ -1,13 +1,13 @@
 # Draw a circle
 # 1BHIF | Marc Edlinger | 31.01.2022
-size: int = 15
-rad: int = 6
-values: list = [["" for x in range(size)] for y in range(size)]  # fill the array with space strings
+rad: int = 4            # radius of the circle
+diameter: int = 2*rad
+values: list = [["" for x in range(diameter)] for y in range(diameter)]  # fill the array with space strings
 
-for y in range(size):  # put valid y,x values in the map
-    for x in range(size):
-        if ((x - rad) ** 2 + (y - rad) ** 2 <= rad ** 2 - 1):
-            values[y][x] = "*"
+for y in range(diameter):  # put valid y,x values in the map
+    for x in range(diameter):
+        if ((y - rad)**2 + (x - rad)**2 == rad**2):
+            print(f"True for ({x} | {y})")
 
 for line in values:
     print(" ".join(line))
