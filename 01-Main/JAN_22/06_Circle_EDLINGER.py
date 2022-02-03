99f464185e3ae01fc63ec6d2c82b78d41aac4fc8
Marc Edlinger <marc.edlinger@edu.htl-villach.at>
2022-02-03 17:49:29 +0100

    worked on cirlce

--- 01-Main/JAN_22/06_Circle_EDLINGER.py
+++ 01-Main/JAN_22/06_Circle_EDLINGER.py
@@ -6,8 +6,8 @@ values: list = [["" for x in range(diameter)] for y in range(diameter)]  # fill 
 
 for y in range(diameter):  # put valid y,x values in the map
     for x in range(diameter):
-        if ((y - rad)**2 + (x - rad)**2 == rad**2):
-            print(f"True for ({x} | {y})")
+        if ((y - rad)**2 + (x - rad)**2 <= rad**2 - 1):
+            values[y][x] = "x"
 
 for line in values:
     print(" ".join(line))
