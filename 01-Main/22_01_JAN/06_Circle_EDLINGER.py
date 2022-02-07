be9da29483f0cb3d69d248fa59125e5adbea5d9c
Forumat <forumat.kontakt@gmail.com>
2022-02-07 12:57:50 +0100

    worked on circle

--- 01-Main/22_01_JAN/06_Circle_EDLINGER.py
+++ 01-Main/22_01_JAN/06_Circle_EDLINGER.py
@@ -1,13 +1,17 @@
 # Draw a circle
 # 1BHIF | Marc Edlinger | 31.01.2022
+from math import floor
 rad: int = 4            # radius of the circle
-diameter: int = 2*rad
+diameter: int = 2*rad + 1
 values: list = [["" for x in range(diameter)] for y in range(diameter)]  # fill the array with space strings
 
-for y in range(diameter):  # put valid y,x values in the map
-    for x in range(diameter):
-        if ((y - rad)**2 + (x - rad)**2 <= rad**2 - 1):
-            values[y][x] = "x"
+for x, s in enumerate(values):
+    for y, s1 in enumerate(values):
+        if ((x - rad)**2 + y**2 <= rad**2):
+            values[x][y] = "x"
 
 for line in values:
-    print(" ".join(line))
+    line_spaces = " " 
+    for entry in line:
+        print(rad*entry, end="")
+    print("")
