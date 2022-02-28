4689d5c031664e61273a1c9964630678f89622ac
edlingem <edlingem@edu.htl-villach.at>
2022-02-28 12:15:02 +0100

    code formattings

--- 02-Fun/ListSorter.py
+++ 02-Fun/ListSorter.py
@@ -1,7 +1,7 @@
 import random as rndm
-import time
 
 rndm.seed()
+
 
 def is_sorted(to_check: list) -> bool:
     for i in range(len(to_check) - 1):
@@ -9,15 +9,13 @@ def is_sorted(to_check: list) -> bool:
             return False
     return True
 
+
 def sort(list: list) -> int:
-    start = time.time() * 1000
     tries: int = 0
 
     while not is_sorted(list):
         i: int = rndm.randrange(len(list))
         j: int = rndm.randrange(len(list))
-        #while j == i:
-        #    j = rndm.randrange(len(list))
         tmp: int = list[i]
         list[i] = list[j]
         list[j] = tmp
@@ -25,12 +23,10 @@ def sort(list: list) -> int:
         if tries % 5000000 == 0:
             print(f"Zwischenstand {tries:,}")
 
-    stop = time.time() * 1000
     print(f"Liste mit {len(list)} Elementen is jetzt sortiert! Gebrauchte Versuche: {tries:,}")
     print(" " * 100)
-    # print("Dauerte", (stop - start), "ms")
-    # print(list)
     return tries
+
 
 runs: int = 5
 for i in range(runs):
