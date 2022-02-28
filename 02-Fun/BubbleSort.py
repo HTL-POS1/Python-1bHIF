c2ee2a79e0625262f844a0d4b2286120a7549158
edlingem <edlingem@edu.htl-villach.at>
2022-02-28 13:50:23 +0100

    created bubble sort file

---
+++ 02-Fun/BubbleSort.py
@@ -0,0 +1,7 @@
+def sort(to_sort: list) -> list:
+    """ Sorts an provided list, and return it back """
+    sorted_list: list = []
+    pass
+
+
+test_1: list = [5, 2, 7, 8] # expected output: [2, 4, 7, 8]
