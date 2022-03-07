bb51484977731e9ddb03df7c17bb62b114a7122a
edlingem <edlingem@edu.htl-villach.at>
2022-03-07 13:00:12 +0100

    add documentation

--- 01-Main/22_03_MAR/06_Edlinger_CallByRef_19.py
+++ 01-Main/22_03_MAR/06_Edlinger_CallByRef_19.py
@@ -2,6 +2,7 @@
 # 2. Aufgabe mit Listen in python
 
 
+# unverändert
 def getPosition(max: int) -> int:
     """ liest vom Anwender eine gueltige (0 bis max) Position ein und liefert diese """
     pos = int(input("wo willst du die Zahl einfügen (0-" + str(max) + ") "))
@@ -10,6 +11,7 @@ def getPosition(max: int) -> int:
     return pos
 
 
+# unverändert
 def getValue() -> int:
     """ liest vom Anwender eine positive Zahl ein und liefert diese """
     z = int(input("gib mir eine Zahl, die du einfügen willst "))
@@ -18,6 +20,7 @@ def getValue() -> int:
     return z
 
 
+#
 def readList(liste: list, list_len: int):
     """ liest einen neuen Wert in die Liste liste der Länge list_len ein und liefert diese Liste """
     st = getPosition(list_len-1)    # hol die Position zum Einfügen
