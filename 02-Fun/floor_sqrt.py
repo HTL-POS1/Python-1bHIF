9bfd5d2ac3576872f9064bc5dc970a1c9148296a
edlingem <edlingem@edu.htl-villach.at>
2022-03-14 08:25:29 +0100

    implemented complex number support

--- 02-Fun/floor_sqrt.py
+++ 02-Fun/floor_sqrt.py
@@ -5,27 +5,22 @@ calculate int sqrt of an integer
 """
 
 
-def sqrt(n: int) -> int:
+def sqrt(n: int) -> complex:
     if (n < 0):
-        raise ArithmeticError("cannot take square root of negative numbers in R")
+        return complex(0, sqrt(n * -1).imag)
+    if (n == -1):
+        return complex(0, 1)
     if (n == 0 or n == 1):
         return n
 
     count: int = 0
     while (count < n):
         if (count**2 > n):
-            return count - 1
+            return complex(0, count - 1)
         count += 1
     return -1
 
 
-def test(n: int):
-    print(n, "|", sqrt(n))
-
-
-test(0)
-test(1)
-test(12)
-test(16)
-test(4)
-test(3)
+while True:
+    i: int = int(input("Zahl: "))
+    print(i, "|", sqrt(i))
