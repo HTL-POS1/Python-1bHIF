8d9f1a05664b938f7378f4527e1ec7b71d391946
edlingem <edlingem@edu.htl-villach.at>
2022-03-14 14:28:09 +0100

    improved quick sort

--- 02-Fun/sorters/QuickSort.py
+++ 02-Fun/sorters/QuickSort.py
@@ -6,7 +6,6 @@ Quick sort implementation in python
 
 
 def partition(list: list, start: int, to: int) -> int:
-    """ returns the middle index of a list """
     i = start - 1                                           # index of smaller element
     last_element = list[to]                             # last_element
 
@@ -15,19 +14,18 @@ def partition(list: list, start: int, to: int) -> int:
             i += 1
             list[i], list[j] = list[j], list[i]         # switch
 
-    list[i + 1], list[to] = list[to], list[i + 1]       # switch
-    return (i + 1)
+    i_ = (i + 1)
+    list[i_], list[to] = list[to], list[i_]       # switch
+    return i_
 
 
-def quick_sort(list: list, start, to):
+def quick_sort(list: list, start: int, to: int):
     """ quickly sort a list recursively """
     if len(list) == 1:
         return list
     if (start < to):
         middle_index = partition(list, start, to)
 
-        # Separately sort elements before
-        # partition and after partition
         quick_sort(list, start, middle_index - 1)
         quick_sort(list, middle_index + 1, to)
 
