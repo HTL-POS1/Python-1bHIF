95d4dc46d4f8e377ed5f6eedb27ed55d24ded6e1
edlingem <edlingem@edu.htl-villach.at>
2022-03-17 09:14:33 +0100

    added tests

--- 01-Main/22_03_MAR/06_Edlinger_Lists_Aufg21.py
+++ 01-Main/22_03_MAR/06_Edlinger_Lists_Aufg21.py
@@ -30,3 +30,18 @@ def compare(first: list, second: list) -> int:
         if (first[i] == second[i]):
             same += 1
     return same
+
+
+if __name__ == '__main__':
+    list1 = [1, 2, 3]
+    list2 = [1, 4, 5]
+    list3 = [0, 4, 5]
+    list4 = [1, 2, 3, 4]
+    list5 = [0, 0, 0, 0]
+    list6 = []
+
+    print(count(list1, 2), count(list5, 0), count(list6, 3))  # -> 1 4 0
+    copy(list1, list5)
+    copy(list2, list6)
+    print(list5, list6)  # -> [1, 2, 3, 0] []
+    print(compare(list1, list2), compare(list2, list4), compare(list4, list5))  # -> 1 1 3
