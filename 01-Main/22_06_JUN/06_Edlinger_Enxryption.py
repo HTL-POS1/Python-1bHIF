162945f60a5da6a58231be58e928fcd0fa7ca110
edlingem <edlingem@edu.htl-villach.at>
2022-06-02 09:37:20 +0200

    encrypt function improved

--- 01-Main/22_06_JUN/06_Edlinger_Enxryption.py
+++ 01-Main/22_06_JUN/06_Edlinger_Enxryption.py
@@ -4,25 +4,38 @@ Marc EDLINGER
 
 String encryption
 """
-BIG_Z = 90
-SMALL_A = 97
-SMALL_Z = 122
-BIG_A = 65
-
+CAPITAL_ABC = range(65, 91)
+LOWER_ABC = range(97, 123)
+ABC_LEN = len(CAPITAL_ABC)
 
 def encrypt(s: str, secret: int) -> str:
     encrypted: str = ""
+
     for c in s:
         code: int = ord(c)
-        code += secret
-        if code >= SMALL_A:                                     # kleinbuchstabe
-            if code > SMALL_Z:                                  # > z
-                code = (code - SMALL_Z) + SMALL_A - 1
-        else:                                                   # grossbuchstabe
-            if code > BIG_Z:                                    # > Z
-                code = (code - SMALL_Z) + BIG_A - 1
-        encrypted += chr(code)
+        if code in CAPITAL_ABC:
+            encrypted_char = chr(CAPITAL_ABC[get_new_index(CAPITAL_ABC, code, secret)])
+        else:
+            encrypted_char = chr(LOWER_ABC[get_new_index(LOWER_ABC, code, secret)])
+
+        encrypted += encrypted_char
+
     return encrypted
+
+
+def decrypt(s: str, secret: int) -> str:
+    return encrypt(s, -secret)
+
+
+def get_new_index(abc: list[int], origin: int, secret: int):
+    i: int = abc.index(origin) + secret
+    list_len = len(abc)
+    if i < 0:
+        i = list_len + i
+    elif i >= list_len:
+        i %= list_len
+
+    return i
 
 
 def change_mutation_chars(s: str) -> str:
